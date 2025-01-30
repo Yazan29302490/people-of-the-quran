cd5337f1b7ff06e4667b3199e865433f0afd1550
omarx11 <igamerxd3@gmail.com>
2025-01-31 00:47:02 +0300

    fixed dark mode toggle.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,25 @@ import Header from "./Component/Header/Header.jsx";
 import Main from "./Component/Main/Main.jsx";
 import Robot from "./Component/Robot/Robot.jsx";
 import Miscellaneous from "./Component/Miscellaneous/Miscellaneous.jsx";
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  // trigger dark mode on load
+  useEffect(() => {
+    const body = document.querySelector("body");
+    const checkbox = document.getElementById("checkbox");
+
+    const isDarkMode = JSON.parse(localStorage.getItem("darkMode")) === true;
+    if (isDarkMode) {
+      body.classList.add("active");
+      checkbox.checked = true;
+    } else {
+      body.classList.remove("active");
+      checkbox.checked = false;
+    }
+  }, []);
+
   return (
     <div className="App">
       <div className="co-header-main">
@@ -20,7 +36,7 @@ function App() {
         <Miscellaneous></Miscellaneous>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
